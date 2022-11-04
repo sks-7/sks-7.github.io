16b69f5e99b331d0086bb53f10840a7f2d0c5c85
sks-7 <sachinsinghofficial7@gmail.com>
2022-11-04 19:58:07 +0530

    added 4th project and navbar added resume download icon

--- src/Componetns/Nav/Nav.jsx
+++ src/Componetns/Nav/Nav.jsx
@@ -6,20 +6,64 @@ import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageSquareDetail} from 'react-icons/bi'
 import { useState } from 'react'
+import CV from '../../Assests/Sachin_Kumar_Resume.pdf';
+import { TiDocumentText } from 'react-icons/ti';
 
 const Nav = () => {
   const [activenav,setActivenav] = useState("#")
   return (
-    <div className='nav_content'>
-    <nav>
-      <a href="#" onClick={()=>setActivenav("#")} className={activenav==="#" ? "active" : ""}><AiOutlineHome/></a>
-      <a href="#about" onClick={()=>setActivenav("#about")} className={activenav==="#about" ? "active" : ""}><AiOutlineUser/></a>
-      <a href="#experience" onClick={()=>setActivenav("#experience")} className={activenav==="#experience" ? "active" : ""}><BiBook/></a>
-      <a href="#skill" onClick={()=>setActivenav("#skill")} className={activenav==="#skill" ? "active" : ""}><RiServiceLine/></a>
-      <a href="#contact" onClick={()=>setActivenav("#contact")} className={activenav==="#contact" ? "active" : ""}><BiMessageSquareDetail/></a>
-    </nav>
+    <div className="nav_content">
+      <nav>
+        <a
+          href="#"
+          onClick={() => setActivenav('#')}
+          className={activenav === '#' ? 'active' : ''}
+        >
+          <AiOutlineHome />
+        </a>
+        <a
+          href="#about"
+          onClick={() => setActivenav('#about')}
+          className={activenav === '#about' ? 'active' : ''}
+        >
+          <AiOutlineUser />
+        </a>
+        {/* <a
+          href="#experience"
+          onClick={() => setActivenav('#experience')}
+          className={activenav === '#experience' ? 'active' : ''}
+        >
+          <BiBook />
+        </a> */}
+        <a
+          href="#projects"
+          onClick={() => setActivenav('#projects')}
+          className={activenav === '#projects' ? 'active' : ''}
+        >
+          <BiBook />
+        </a>
+
+        <a
+          href="#skill"
+          onClick={() => setActivenav('#skill')}
+          className={activenav === '#skill' ? 'active' : ''}
+        >
+          <RiServiceLine />
+        </a>
+        <a
+          href="#contact"
+          onClick={() => setActivenav('#contact')}
+          className={activenav === '#contact' ? 'active' : ''}
+        >
+          <BiMessageSquareDetail />
+        </a>
+
+        <a href={CV} download>
+          <TiDocumentText />
+        </a>
+      </nav>
     </div>
-  )
+  );
 }
 
 export default Nav
