7429f42d7f9ecad007b2719694167a119924d1c3
sks-7 <sachinsinghofficial7@gmail.com>
2023-04-28 21:53:53 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React from "react";
-import Header from './Componetns/Header/Header'
-import Nav from './Componetns/Nav/Nav'
-import About from './Componetns/About/About'
-import Experience from './Componetns/Experience/Experience'
+import React from 'react';
+import Header from './Componetns/Header/Header';
+import Nav from './Componetns/Nav/Nav';
+import About from './Componetns/About/About';
+import Experience from './Componetns/Experience/Experience';
 // import Servises from './Componetns/Services/Servises'
-import Protfolio from './Componetns/Protfolio/Protfolio'
-import Testimonial from './Componetns/Testimonials/Testimonial'
-import Contact from './Componetns/Contact/Contact'
-import Footer from './Componetns/Footer/Footer'
-import Services from "./Componetns/Services/Servises";
-import Github from "./Componetns/GithubCalender/githubCalender";
-
+import Protfolio from './Componetns/Protfolio/Protfolio';
+import Testimonial from './Componetns/Testimonials/Testimonial';
+import Contact from './Componetns/Contact/Contact';
+import Footer from './Componetns/Footer/Footer';
+import Services from './Componetns/Services/Servises';
+import Github from './Componetns/GithubCalender/githubCalender';
 
 function App() {
   return (
-   <>
-      <Header/>
-      <Nav/>
-      <About/>
-      <Experience/>
-      <Protfolio/>
-      <Services/>
-      <Github/>
-      <Testimonial/>
-      <Contact/>
-      <Footer/>
-   </>
+    <>
+      <Nav />
+      <Header />
+      <About />
+      <Experience />
+      <Protfolio />
+      <Services />
+      <Github />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
